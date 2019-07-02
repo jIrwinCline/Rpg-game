e93e5b296d865be83a58ff53ff6cea5c12079515
jirwincline <jakobinwins@gmail.com>
2019-07-01 22:28:17 -0700

    make each click create its own character

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Character } from './battleMaster.js'
 
 $(document).ready(function () {
+  let user = new Character();
   $('#warrior').click(function() {
-    const warrior = new Character('The Warrior', 100, 50)
+    user = new Character('The Warrior', 100, 50)
     $('.characters').hide();
-});
+  });
+  $('#archer').click(function() {
+    user = new Character('The Archer', 90, 60)
+    $('.characters').hide();
 
+  });
+  $('#wizard').click(function() {
+    user = new Character('The Wizard', 120, 40)
+    $('.characters').hide();
+  });
+  $(".characters").on("click", function(){
+  console.log(user);
+});
 
 
 });
